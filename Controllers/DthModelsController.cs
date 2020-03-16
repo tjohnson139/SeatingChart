24a6cb8b065570322f3d0601817a88fef666869d
Tyler Johnson <tylerjohnson139@gmail.com>
2020-03-16 02:57:15 -0700

    add dth column

---
+++ Controllers/DthModelsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SeatingChart.Models;
+
+namespace SeatingChart
+{
+    public class DthModelsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: DthModels
+        public ActionResult Index()
+        {
+            var dthModels = db.DthModels.Include(d => d.EmployeeModels);
+            return View(dthModels.ToList());
+        }
+
+        // GET: DthModels/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DthModels dthModels = db.DthModels.Find(id);
+            if (dthModels == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dthModels);
+        }
+
+        // GET: DthModels/Create
+        public ActionResult Create()
+        {
+            ViewBag.DthEmployee = new SelectList(db.EmployeeModels, "Id", "FirstName");
+            return View();
+        }
+
+        // POST: DthModels/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "DthId,DthEmployee,DthTimeEntered,DthTimeCleared")] DthModels dthModels)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DthModels.Add(dthModels);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DthEmployee = new SelectList(db.EmployeeModels, "Id", "FirstName", dthModels.DthEmployee);
+            return View(dthModels);
+        }
+
+        // GET: DthModels/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DthModels dthModels = db.DthModels.Find(id);
+            if (dthModels == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DthEmployee = new SelectList(db.EmployeeModels, "Id", "FirstName", dthModels.DthEmployee);
+            return View(dthModels);
+        }
+
+        // POST: DthModels/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "DthId,DthEmployee,DthTimeEntered,DthTimeCleared")] DthModels dthModels)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(dthModels).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DthEmployee = new SelectList(db.EmployeeModels, "Id", "FirstName", dthModels.DthEmployee);
+            return View(dthModels);
+        }
+
+        // GET: DthModels/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DthModels dthModels = db.DthModels.Find(id);
+            if (dthModels == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dthModels);
+        }
+
+        // POST: DthModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            DthModels dthModels = db.DthModels.Find(id);
+            db.DthModels.Remove(dthModels);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Migrations/202003150540393_Initial.cs
@@ -0,0 +1,144 @@
+namespace SeatingChart.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.BreakModels",
+                c => new
+                    {
+                        BreakId = c.Int(nullable: false, identity: true),
+                        Employee = c.Int(nullable: false),
+                        TimeEntered = c.DateTime(nullable: false),
+                        TimeCleared = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.BreakId)
+                .ForeignKey("dbo.EmployeeModels", t => t.Employee, cascadeDelete: true)
+                .Index(t => t.Employee);
+
+            CreateTable(
+                "dbo.DthModels",
+                c => new
+                {
+                    DthId = c.Int(nullable: false, identity: true),
+                    DthEmployee = c.Int(nullable: false),
+                    DthTimeEntered = c.DateTime(nullable: false),
+                    DthTimeCleared = c.DateTime(),
+                })
+                .PrimaryKey(t => t.DthId)
+                .ForeignKey("dbo.EmployeeModels", t => t.DthEmployee, cascadeDelete: true)
+                .Index(t => t.DthEmployee);
+
+            CreateTable(
+                "dbo.EmployeeModels",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(nullable: false, maxLength: 15),
+                        LastName = c.String(nullable: false, maxLength: 20),
+                        DisplayName = c.String(nullable: false, maxLength: 15),
+                        NotActive = c.Boolean(nullable: false),
+                        Force = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+
+            CreateTable(
+                "dbo.AspNetRoles",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Name, unique: true, name: "RoleNameIndex");
+            
+            CreateTable(
+                "dbo.AspNetUserRoles",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.UserId, t.RoleId })
+                .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId)
+                .Index(t => t.RoleId);
+            
+            CreateTable(
+                "dbo.AspNetUsers",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        Email = c.String(maxLength: 256),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumber = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.UserName, unique: true, name: "UserNameIndex");
+            
+            CreateTable(
+                "dbo.AspNetUserClaims",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserLogins",
+                c => new
+                    {
+                        LoginProvider = c.String(nullable: false, maxLength: 128),
+                        ProviderKey = c.String(nullable: false, maxLength: 128),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            DropForeignKey("dbo.BreakModels", "Employee", "dbo.EmployeeModels");
+            DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
+            DropIndex("dbo.AspNetUsers", "UserNameIndex");
+            DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
+            DropIndex("dbo.AspNetRoles", "RoleNameIndex");
+            DropIndex("dbo.BreakModels", new[] { "Employee" });
+            DropTable("dbo.AspNetUserLogins");
+            DropTable("dbo.AspNetUserClaims");
+            DropTable("dbo.AspNetUsers");
+            DropTable("dbo.AspNetUserRoles");
+            DropTable("dbo.AspNetRoles");
+            DropTable("dbo.EmployeeModels");
+            DropTable("dbo.BreakModels");
+        }
+    }
+}
